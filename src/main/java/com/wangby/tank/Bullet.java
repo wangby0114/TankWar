3070499f2c3164b6ccfb20adb6354e9be5022f45
wangby0114 <30586621+wangby0114@users.noreply.github.com>
2020-03-26 19:55:12 +0800

    Update Bullet.java

--- src/main/java/com/wangby/tank/Bullet.java
+++ src/main/java/com/wangby/tank/Bullet.java
@@ -7,6 +7,8 @@ public class Bullet {
     private final int SPEED = Integer.parseInt(props.get("bulletSpeed"));
     private int x, y;
     private Dir dir = Dir.VK_DOWN;
+    
+    
 
     private boolean living = true;
     private TankFrame tf;
